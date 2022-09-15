56e9e7fb2f21e3a65ce52c4c2f5bdca7736a6035
gedwards1988 <108200463+gedwards1988@users.noreply.github.com>
2022-09-15 23:55:42 +0100

    uPDATE

---
+++ Encapsulation/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace Encapsulation
+{
+    class Person
+    {
+        private int _id;
+        private string _userName;
+        private string _password;
+
+
+        public int ID
+        {
+           get { return _id; }
+            set { _id = value; }
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public Person(int Id)
+        {
+            ID = Id;
+        }
+
+
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Encapsulation
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(1);
+            person.UserName = "Gareth Edwards";
+            person.Password = "XYZ1123";
+        }
+    }
+}
